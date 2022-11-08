e5db574edab91f5b3fbb5f8b3b2fffd40fbc8987
Guillaume RICARD <guillaume.ricard@telecomnancy.eu>
2022-11-08 10:36:04 +0100

    Avec ma mère, faut que tout soit carré.

--- src/filters.c
+++ src/filters.c
@@ -1,92 +1,92 @@
+#include "filters.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "filters.h"
 
-Filter_list *create_filter_list() {
-    Filter_list *list = malloc(sizeof(Filter_list));
-    list->data = NULL;
-    list->next = NULL;
-    return list;
+FilterList *create_filter_list() {
+  FilterList *list = malloc(sizeof(FilterList));
+  list->data = NULL;
+  list->next = NULL;
+  return list;
 }
 
-void destroy_filter_list(Filter_list *list) {
-    Filter_list *tmp;
-    while (list != NULL) {
-        tmp = list;
-        list = list->next;
+void destroy_filter_list(FilterList *list) {
+  FilterList *tmp;
+  while (list != NULL) {
+    tmp = list;
+    list = list->next;
+    free(tmp->data);
+    free(tmp);
+  }
+}
+
+void add_filter(FilterList *list, filter_type type, void *value) {
+  if (list->data == NULL) {
+    FilterData *data = malloc(sizeof(FilterData));
+    data->type = type;
+    data->value = value;
+    list->data = data;
+  } else {
+    FilterList *tmp = list;
+    while (tmp->next != NULL) {
+      tmp = tmp->next;
+    }
+    FilterList *next_filter = create_filter_list();
+    add_filter(next_filter, type, value);
+    tmp->next = next_filter;
+  }
+}
+
+void remove_filter(FilterList *list, filter_type type) {
+  if (list->data == NULL) {
+    return;
+  } else {
+    FilterList *tmp = list;
+    while (tmp != NULL) {
+      if (tmp->data->type == type) {
+        FilterList *next = tmp->next;
         free(tmp->data);
         free(tmp);
+        tmp = next;
+      } else {
+        tmp = tmp->next;
+      }
     }
+  }
 }
 
-void add_filter(Filter_list *list, filter_type type, void *value) {
-    if (list->data == NULL) {
-        Filter_data *data = malloc(sizeof(Filter_data));
-        data->type = type;
-        data->value = value;
-        list->data = data;
-    } else {
-        Filter_list *tmp = list;
-        while (tmp->next != NULL) {
-            tmp = tmp->next;
-        }
-        Filter_list *next_filter = create_filter_list();
-        add_filter(next_filter, type, value);
-        tmp->next = next_filter;
-    }
-}
-
-void remove_filter(Filter_list *list, filter_type type) {
-    if (list->data == NULL) {
-        return;
-    }
-    else{
-        Filter_list *tmp = list;
-        while (tmp != NULL) {
-            if (tmp->data->type == type) {
-                Filter_list *next = tmp->next;
-                free(tmp->data);
-                free(tmp);
-                tmp = next;
-            } else {
-                tmp = tmp->next;
-            }
-        }
-    }
-}
-
-bool filter_match(char *filename, Filter_data data) {
-    switch (data.type) {
-        case NAME:
-            return strcmp(filename, data.value) == 0;
-        case SIZE_EQ:
-            // TODO
-            return false;
-        case SIZE_GT:
-            // TODO
-            return false;
-        case SIZE_LT:
-            // TODO
-            return false;
-        case DATE_GT:
-            // TODO
-            return false;
-        case DATE_LT:
-            // TODO
-            return false;
-        case MIME:
-            // TODO
-            return false;
-        case CTC:
-            // TODO
-            return false;
-        default:
-            return false;
-    }
-}
-
-bool filter_logical_match(enum {AND, OR} logical, char *filename, Filter_list *list) {
+bool filter_match(char *filename, FilterData *data) {
+  switch (data->type) {
+  case FILTER_NAME:
+    return strcmp(filename, data->value) == 0;
+  case FILTER_SIZE_EQ:
     // TODO
     return false;
+  case FILTER_SIZE_GT:
+    // TODO
+    return false;
+  case FILTER_SIZE_LT:
+    // TODO
+    return false;
+  case FILTER_DATE_GT:
+    // TODO
+    return false;
+  case FILTER_DATE_LT:
+    // TODO
+    return false;
+  case FILTER_MIME:
+    // TODO
+    return false;
+  case FILTER_CTC:
+    // TODO
+    return false;
+  default:
+    return false;
+  }
+}
+
+bool filter_logical_match(logical_op logical, char *filename,
+                          FilterList *list) {
+  // TODO
+  return true;
 }
--- src/filters.h
+++ src/filters.h
@@ -3,24 +3,26 @@
 
 #include <stdbool.h>
 
-typedef enum {NAME, SIZE_EQ, SIZE_GT, SIZE_LT, DATE_GT, DATE_LT, MIME, CTC} filter_type;
+typedef enum {FILTER_NAME, FILTER_SIZE_EQ, FILTER_SIZE_GT, FILTER_SIZE_LT, FILTER_DATE_GT, FILTER_DATE_LT, FILTER_MIME, FILTER_CTC} filter_type;
 
-typedef struct _Filter_data {
+typedef enum {AND, OR} logical_op;
+
+typedef struct _FilterData {
     filter_type type;
     void *value;
-} Filter_data;
+} FilterData;
 
-typedef struct _Filter_list {
-    Filter_data *data;
-    struct _Filter_list *next;
-} Filter_list;
+typedef struct _FilterList {
+    FilterData *data;
+    struct _FilterList *next;
+} FilterList;
 
-Filter_list *create_filter_list();
-void destroy_filter_list(Filter_list *list);
-void add_filter(Filter_list *list, filter_type type, void *value);
-void remove_filter(Filter_list *list, filter_type type);
-bool filter_match(char *filename, Filter_data data);
-bool filter_logical_match(enum {AND, OR} logical, char *filename, Filter_list *list);
+FilterList *create_filter_list();
+void destroy_filter_list(FilterList *list);
+void add_filter(FilterList *list, filter_type type, void *value);
+void remove_filter(FilterList *list, filter_type type);
+bool filter_match(char *filename, FilterData *data);
+bool filter_logical_match(logical_op logical, char *filename, FilterList *list);
 
 
 #endif // _FILTERS_H_
