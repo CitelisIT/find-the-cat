36f1d123167fc895e4fa159a9bb2e893acc641ce
Guillaume RICARD <guillaume.ricard@telecomnancy.eu>
2022-11-08 12:29:06 +0100

    Non, mon nom ne vous dira rien... Mais chez vous on m'appelle... La réponse

--- src/filters.h
+++ src/filters.h
@@ -3,18 +3,27 @@
 
 #include <stdbool.h>
 
-typedef enum {FILTER_NAME, FILTER_SIZE_EQ, FILTER_SIZE_GT, FILTER_SIZE_LT, FILTER_DATE_GT, FILTER_DATE_LT, FILTER_MIME, FILTER_CTC} filter_type;
+typedef enum {
+  FILTER_NAME,
+  FILTER_SIZE_EQ,
+  FILTER_SIZE_GT,
+  FILTER_SIZE_LT,
+  FILTER_DATE_GT,
+  FILTER_DATE_LT,
+  FILTER_MIME,
+  FILTER_CTC
+} filter_type;
 
-typedef enum {AND, OR} logical_op;
+typedef enum { AND, OR } logical_op;
 
 typedef struct _FilterData {
-    filter_type type;
-    void *value;
+  filter_type type;
+  void *value;
 } FilterData;
 
 typedef struct _FilterList {
-    FilterData *data;
-    struct _FilterList *next;
+  FilterData *data;
+  struct _FilterList *next;
 } FilterList;
 
 FilterList *create_filter_list();
@@ -24,5 +33,6 @@ void remove_filter(FilterList *list, filter_type type);
 bool filter_match(char *filename, FilterData *data);
 bool filter_logical_match(logical_op logical, char *filename, FilterList *list);
 
+bool filter_name(char *filename, char *value);
 
 #endif // _FILTERS_H_
