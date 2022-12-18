68a4716b4791ebfac84fb85a4f087f1f30b2749b
Tom BÉNÉ <tom.bene@telecomnancy.eu>
2022-12-18 21:27:14 +0100

    Marre de cette compagnie de branleurs !

--- src/filters.c
+++ src/filters.c
@@ -1,13 +1,12 @@
 #define _ATFILE_SOURCE
 
-
 #include "filters.h"
 #include "../lib/MegaMimes.h"
 #include "context.h"
 #include "filesystem.h"
 #include "flags.h"
 #include <ctype.h>
-#include <fcntl.h>  
+#include <fcntl.h>
 #include <fts.h>
 #include <regex.h>
 #include <stdio.h>
@@ -342,104 +341,70 @@ bool filter_name(char *filename, char *value) {
 
 bool filter_size_eq(char *path, long value) {
   struct stat file_stat;
-  if (stat(path, &file_stat) == -1) {
+  if (fstatat(AT_FDCWD, path, &file_stat,
+              app_context.follow_links ? 0 : AT_SYMLINK_NOFOLLOW) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
-  }
-  if (S_ISLNK(file_stat.st_mode)) {
-    if (fstatat(AT_FDCWD, path, &file_stat, 0) == -1) {
-      fprintf(stderr, "Error while getting file stats \n");
-      exit(1);
-    }
-    return file_stat.st_size == value;
   }
   return file_stat.st_size == value;
 }
 
 bool filter_size_gt(char *path, long value) {
   struct stat file_stat;
-  if (stat(path, &file_stat) == -1) {
+  if (fstatat(AT_FDCWD, path, &file_stat,
+              app_context.follow_links ? 0 : AT_SYMLINK_NOFOLLOW) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
-  }
-  if (S_ISLNK(file_stat.st_mode)) {
-    if (fstatat(AT_FDCWD, path, &file_stat, 0) == -1) {
-      fprintf(stderr, "Error while getting file stats \n");
-      exit(1);
-    }
-    return file_stat.st_size > value;
   }
   return file_stat.st_size > value;
 }
 
 bool filter_size_lt(char *path, long value) {
   struct stat file_stat;
-  if (stat(path, &file_stat) == -1) {
+  if (fstatat(AT_FDCWD, path, &file_stat,
+              app_context.follow_links ? 0 : AT_SYMLINK_NOFOLLOW) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
-  }
-  if (S_ISLNK(file_stat.st_mode)) {
-    if (fstatat(AT_FDCWD, path, &file_stat,0) == -1) {
-      fprintf(stderr, "Error while getting file stats \n");
-      exit(1);
-    }
-    return file_stat.st_size < value;
   }
   return file_stat.st_size < value;
 }
 
 bool filter_date_gt(char *path, time_t date) {
   struct stat file_stat;
-  if (stat(path, &file_stat) == -1) {
+  if (fstatat(AT_FDCWD, path, &file_stat,
+              app_context.follow_links ? 0 : AT_SYMLINK_NOFOLLOW) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
   }
   time_t actual_time = time(NULL);
-
-  if (S_ISLNK(file_stat.st_mode)) {
-    if (fstatat(AT_FDCWD, path, &file_stat, 0) == -1) {
-      fprintf(stderr, "Error while getting file stats \n");
-      exit(1);
-    }
-    return (actual_time - file_stat.st_atime) > date;
-  }
   return (actual_time - file_stat.st_atime) > date;
 }
 
 bool filter_date_lt(char *path, time_t date) {
   struct stat file_stat;
-  if (stat(path, &file_stat) == -1) {
+  if (fstatat(AT_FDCWD, path, &file_stat,
+              app_context.follow_links ? 0 : AT_SYMLINK_NOFOLLOW) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
   }
   time_t actual_time = time(NULL);
-
-  if (S_ISLNK(file_stat.st_mode)) {
-    if (fstatat(AT_FDCWD, path, &file_stat, 0) == -1) {
-      fprintf(stderr, "Error while getting file stats \n");
-      exit(1);
-    }
-    return (actual_time - file_stat.st_atime) < date;
-  }
   return (actual_time - file_stat.st_atime) < date;
 }
 
 bool filter_mime(char *path, char *mimetype) {
-
   struct stat file_stat;
   if (stat(path, &file_stat) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
   }
-  if (S_ISLNK(file_stat.st_mode)){
+  if (S_ISLNK(file_stat.st_mode) && app_context.follow_links) {
     char *real_path = realpath(path, NULL);
-    if (realpath == NULL) {
+    if (real_path == NULL) {
       fprintf(stderr, "Error while getting real path of %s", path);
       exit(1);
     }
-    return filter_mime(realpath, mimetype);
+    return filter_mime(real_path, mimetype);
   }
-
 
   const char *mime = getMegaMimeType(path);
   if (mime == NULL) {
@@ -458,7 +423,7 @@ bool filter_ctc(char *path, char *ctc) {
   stat(path, &path_stat);
 
   if (!S_ISDIR(path_stat.st_mode)) {
-    if (S_ISLNK(path_stat.st_mode)) {
+    if (S_ISLNK(path_stat.st_mode) && app_context.follow_links) {
       char *real_path = realpath(path, NULL);
       if (real_path == NULL) {
         fprintf(stderr, "Error while getting real path of %s", path);
@@ -466,54 +431,53 @@ bool filter_ctc(char *path, char *ctc) {
       }
       return filter_ctc(real_path, ctc);
     } else {
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-      fprintf(stderr, "Error while opening file %s", path);
-      exit(1);
-    }
-
-    fseek(file, 0, SEEK_END);
-    int length = ftell(file);
-
-    fseek(file, 0, SEEK_SET);
-
-    char *buffer = malloc(sizeof(char) * (length + 1));
-
-    char c;
-    int i = 0;
-    while ((c = fgetc(file)) != EOF) {
-      buffer[i] = c;
-      i++;
-    }
-    buffer[i] = '\0';
-    fclose(file);
-
-    if (buffer) {
-      regex_t regex;
-      int match_res;
-      char buff[128];
-
-      if ((match_res = regcomp(&regex, ctc, 0))) {
-        regerror(match_res, &regex, buff, 128);
-        fprintf(stderr, "Regex not compiled : %s\n", buff);
+      FILE *file = fopen(path, "r");
+      if (file == NULL) {
+        fprintf(stderr, "Error while opening file %s", path);
         exit(1);
       }
-      match_res = regexec(&regex, buffer, 0, NULL, 0);
-      regfree(&regex);
-      free(buffer);
-      if (match_res == REG_NOMATCH) {
-        return false;
-      } else {
-        return true;
+
+      fseek(file, 0, SEEK_END);
+      int length = ftell(file);
+
+      fseek(file, 0, SEEK_SET);
+
+      char *buffer = malloc(sizeof(char) * (length + 1));
+
+      char c;
+      int i = 0;
+      while ((c = fgetc(file)) != EOF) {
+        buffer[i] = c;
+        i++;
       }
-  }
-    else {
-      fprintf(stderr, "Error while reading file %s\n", path);
-      exit(1);
+      buffer[i] = '\0';
+      fclose(file);
+
+      if (buffer) {
+        regex_t regex;
+        int match_res;
+        char buff[128];
+
+        if ((match_res = regcomp(&regex, ctc, 0))) {
+          regerror(match_res, &regex, buff, 128);
+          fprintf(stderr, "Regex not compiled : %s\n", buff);
+          exit(1);
+        }
+        match_res = regexec(&regex, buffer, 0, NULL, 0);
+        regfree(&regex);
+        free(buffer);
+        if (match_res == REG_NOMATCH) {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        fprintf(stderr, "Error while reading file %s\n", path);
+        exit(1);
+      }
     }
   }
   return false;
-}
 }
 
 bool filter_dir(char *dirname, char *value) {
@@ -531,18 +495,12 @@ bool filter_dir(char *dirname, char *value) {
 
 bool filter_perms(char *path, long value) {
   struct stat file_stat;
-  if (stat(path, &file_stat) == -1) {
+  if (fstatat(AT_FDCWD, path, &file_stat,
+              app_context.follow_links ? 0 : AT_SYMLINK_NOFOLLOW) == -1) {
     fprintf(stderr, "Error while getting file stats \n");
     exit(1);
   }
   if (!S_ISDIR(file_stat.st_mode)) {
-    if (S_ISLNK(file_stat.st_mode)) {
-    if (fstatat(AT_FDCWD, path, &file_stat, 0) == -1) {
-      fprintf(stderr, "Error while getting file stats \n");
-      exit(1);
-    }
-    return ((long)(file_stat.st_mode & 0xfff) == value);
-  }
     return ((long)(file_stat.st_mode & 0xfff) == value);
   } else {
     return false;
